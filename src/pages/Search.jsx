e56aad324a70ca97fc085ab78817a59cd3294276
Gabriel Duarte <gabrielduarte1405@gmail.com>
2022-08-30 18:15:34 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,32 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    nomeArtista: '',
+    // botao: false,
+  };
+
   render() {
+    const { nomeArtista } = this.state;
+    const MIN_LENGTH = 2;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <form>
+          <input
+            type="text"
+            placeholder="Nome do Artista"
+            data-testid="search-artist-input"
+            onChange={ ({ target }) => this.setState({ nomeArtista: target.value }) }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ nomeArtista.length < MIN_LENGTH }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
