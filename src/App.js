a145ef6fceaa84bd76bb05b4ea09ee1ee41ca93e
Gabriel Duarte <gabrielduarte1405@gmail.com>
2022-08-30 15:10:12 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
