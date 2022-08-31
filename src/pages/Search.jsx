9f2f422f8f134f2d3d0f3ad0b1effd32feb60e67
Gabriel Duarte <gabrielduarte1405@gmail.com>
2022-08-31 16:35:29 -0300

    Requisito 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 import Header from '../components/Header';
+// import Loading from './Loading';
 
 class Search extends React.Component {
   state = {
     nomeArtista: '',
-    // botao: false,
+    album: [],
+    // loading: false,
+    nomeAlbum: '',
+  };
+
+  handleClick = () => {
+    const { nomeArtista } = this.state;
+    this.setState({ nomeArtista: '' }, async () => {
+      const album = await searchAlbumsAPI(nomeArtista);
+      this.setState({
+        nomeAlbum: nomeArtista,
+        album,
+        nomeArtista: '',
+        // loading: false,
+      });
+    });
   };
 
   render() {
-    const { nomeArtista } = this.state;
+    const { nomeArtista, album, nomeAlbum } = this.state;
     const MIN_LENGTH = 2;
     return (
       <div data-testid="page-search">
@@ -18,15 +36,47 @@ class Search extends React.Component {
             type="text"
             placeholder="Nome do Artista"
             data-testid="search-artist-input"
-            onChange={ ({ target }) => this.setState({ nomeArtista: target.value }) }
+            value={ nomeArtista }
+            onChange={ ({ target }) => this.setState({
+              nomeArtista: target.value,
+            }) }
           />
           <button
-            type="button"
+            type="submit"
             data-testid="search-artist-button"
+            onClick={ this.handleClick }
             disabled={ nomeArtista.length < MIN_LENGTH }
           >
             Pesquisar
           </button>
+          {!album.length ? 'Nenhum álbum foi encontrado' : (
+            <div>
+              <h3>{`Resultado de álbuns de: ${nomeAlbum}`}</h3>
+              {album.map(({
+                artistName,
+                collectionId,
+                collectionName,
+                collectionPrice,
+                artworkUrl100,
+                releaseDate,
+                trackCount,
+              }) => (
+                <div key={ collectionId }>
+                  <img src={ artworkUrl100 } alt={ artistName } />
+                  <h2>{ artistName }</h2>
+                  <p>{ collectionPrice }</p>
+                  <p>{ trackCount }</p>
+                  <p>{ releaseDate }</p>
+                  <Link
+                    to={ `/album/${collectionId}` }
+                    data-testid={ `link-to-album-${collectionId}` }
+                  >
+                    { collectionName }
+                  </Link>
+                </div>
+              ))}
+            </div>
+          )}
         </form>
       </div>
     );
