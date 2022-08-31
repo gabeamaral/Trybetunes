29fcbbd5fa99da959c532f8ed15ef40d409380e3
Gabriel Duarte <gabrielduarte1405@gmail.com>
2022-08-31 18:42:46 -0300

    Requisito 7

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,15 +1,62 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import getMusics from '../services/musicsAPI';
 
 class Album extends React.Component {
+  state = {
+    musica: [],
+  };
+
+  componentDidMount() {
+    this.musicas();
+  }
+
+  musicas = async () => {
+    const { match: { params: { id } } } = this.props;
+    const song = await getMusics(id);
+    this.setState({ musica: song });
+  };
+
   render() {
+    const { musica } = this.state;
     return (
-      <div data-testid="page-album">
-        <Header />
+      <main data-testid="page-album">
         Album
-      </div>
+        <Header />
+        {musica.length > 0 && (
+          <section>
+            <h5 data-testid="artist-name">
+              {musica[0].artistName}
+            </h5>
+            <h6 data-testid="album-name">
+              {musica[0].collectionName}
+            </h6>
+          </section>
+        )}
+        {
+          musica.length > 0 && (
+            musica.map((e, i) => (
+              i > 0 && (
+                <MusicCard
+                  trackName={ e.trackName }
+                  previewUrl={ e.previewUrl }
+                />
+              )
+            ))
+          )
+        }
+      </main>
     );
   }
 }
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+}.isRequired;
 
 export default Album;
